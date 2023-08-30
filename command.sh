3c7e9b22beaa6fa30e7d1fca32838b62ad61032f
Tony Yan <tony.yan.npr@gmail.com>
2023-08-30 15:40:36 -0400

    second checkin

---
+++ command.sh
@@ -0,0 +1,7 @@
+
+/usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin
+
+/Users/yhappaccount/anaconda3/bin /Users/yhappaccount/anaconda3/condabin /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin
+/Users/yhappaccount/anaconda3/bin:/Users/yhappaccount/anaconda3/condabin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin
+
+/System/Volumes/Data/Users/yhappaccount/xiaodong/VSCode/Visual Studio Code.app
